64fa28ac2b2dabc002f706f05224b8406b7defc5
Angelos <a.plastropoulos.229@cranfield.ac.uk>
2024-09-18 17:44:45 +0100

    Minor updates based on the tutrlebot3 case

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -6,44 +6,66 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
 
 from launch_ros.actions import Node
 
 def generate_launch_description():
 
     package_name='articubot_one'
+    pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+
+    # Launch configuration variables specific to simulation
+    x_pose = LaunchConfiguration('x_pose', default='0.0')
+    y_pose = LaunchConfiguration('y_pose', default='0.0')
+
+    world = os.path.join(
+        get_package_share_directory('articubot_one'),
+        'worlds',
+        'empty_world.world'
+    )
 
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true'}.items()
+                )]), launch_arguments={'use_sim_time': use_sim_time}.items()
     )
 
-    # # Path to the custom SDF world file
-    # world_file_name = 'custom_world.sdf'
-    # world_path = os.path.join(get_package_share_directory(package_name), 'worlds', world_file_name)
+    gzserver_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_gazebo_ros, 'launch', 'gzserver.launch.py')
+        ),
+        launch_arguments={'world': world}.items()
+    )
 
-    # # Check if the custom world file exists
-    # if not os.path.exists(world_path):
-    #     raise FileNotFoundError(f"World file not found: {world_path}")
-
-    # print(f"Using world file: {world_path}")
+    gzclient_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_gazebo_ros, 'launch', 'gzclient.launch.py')
+        )
+    )
 
     # Include the Gazebo launch file, provided by the ros_gz_sim package
-    gazebo = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')]),
-             )
+    # gazebo = IncludeLaunchDescription(
+    #             PythonLaunchDescriptionSource([os.path.join(
+    #                 get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+    #          )
 
     # Run the spawner node from the ros_gz_sim package. The entity name doesn't really matter if you only have a single robot.
-    spawn_entity = Node(package='ros_gz_sim', executable='create',
+    spawn_entity = Node(package='gazebo_ros', 
+                        executable='spawn_entity.py',
                         arguments=['-topic', 'robot_description',
-                                   '-entity', 'my_bot'],
+                                   '-entity', 'my_bot',
+                                   '-x', x_pose,
+                                   '-y', y_pose,
+                                   '-z', '0.01'
+                                   ],
                         output='screen')
 
     # Launch them all!
     return LaunchDescription([
-        #rsp,
-        gazebo,
-        #spawn_entity,
+        rsp,
+        gzserver_cmd,
+        gzclient_cmd,
+        spawn_entity,
     ])
